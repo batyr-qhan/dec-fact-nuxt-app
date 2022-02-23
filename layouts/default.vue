356ffbcc7ee3a7331dff442d8b5a32680f44b9cf
batyrqh <batyrqh@protonmail.com>
2022-02-23 13:36:17 +0300

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid grid-cols-12">
+    <div class="col-span-2"><Sidebar /></div>
+    <div class="col-span-10">
+      <Header />
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "../components/Sidebar.vue";
+import Header from "../components/Header.vue";
+export default {
+  components: { Sidebar, Header },
+  setup() {},
+};
+</script>
